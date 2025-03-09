df2b771f876bd0c87f52659832ea1731aee6530f
yassineBr12 <yassine.berriri@etu.univ-cotedazur.fr>
2025-03-09 12:09:30 +0100

    add local storage

--- front/src/app/services/session.service.ts
+++ front/src/app/services/session.service.ts
@@ -7,10 +7,18 @@ import { SessionInformation } from '../interfaces/SessionInformation.interface';
 })
 export class SessionService {
 
+private readonly SESSION_STORAGE_KEY = 'sessionInfo';
+private readonly IS_LOGGED_KEY = 'isLogged';
+
+
   public isLogged = false;
   public sessionInformation: SessionInformation | undefined;
 
   private isLoggedSubject = new BehaviorSubject<boolean>(this.isLogged);
+
+  constructor() {
+    this.loadSessionFromStorage();
+  }
 
   public $isLogged(): Observable<boolean> {
     return this.isLoggedSubject.asObservable();
@@ -19,16 +27,44 @@ export class SessionService {
   public logIn(user: SessionInformation): void {
     this.sessionInformation = user;
     this.isLogged = true;
+    this.saveSessionToStorage();
     this.next();
   }
 
   public logOut(): void {
     this.sessionInformation = undefined;
     this.isLogged = false;
+    this.clearSessionFromStorage();
     this.next();
   }
 
   private next(): void {
     this.isLoggedSubject.next(this.isLogged);
   }
+
+
+  private saveSessionToStorage(): void {
+    if (this.sessionInformation) {
+      localStorage.setItem(this.SESSION_STORAGE_KEY, JSON.stringify(this.sessionInformation));
+    }
+    localStorage.setItem(this.IS_LOGGED_KEY, JSON.stringify(this.isLogged));
+  }
+
+  private loadSessionFromStorage(): void {
+    const sessionData = localStorage.getItem(this.SESSION_STORAGE_KEY);
+    const isLoggedData = localStorage.getItem(this.IS_LOGGED_KEY);
+    
+    this.isLogged = isLoggedData ? JSON.parse(isLoggedData) : false;
+    
+    if (sessionData && this.isLogged) {
+      this.sessionInformation = JSON.parse(sessionData);
+      this.next();
+    }
+  }
+
+  private clearSessionFromStorage(): void {
+    localStorage.removeItem(this.SESSION_STORAGE_KEY);
+    localStorage.removeItem(this.IS_LOGGED_KEY);
+  }
+
 }
